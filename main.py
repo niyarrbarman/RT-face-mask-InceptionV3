25f6c428896ad0d6e2c676eefb097b88789e58d2
Niyar R Barman <113722156+niyarrbarman@users.noreply.github.com>
2023-01-19 00:36:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,10 +55,8 @@ while True:
 		(startX, startY, endX, endY) = box
 		(mask, withoutMask) = pred
 
-		label = "Mask" if mask > 0.99 else "No Mask"
+		label = "Mask" if mask > withoutMask else "No Mask"
 		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-		# label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
 		cv2.putText(frame, label, (startX, startY - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
